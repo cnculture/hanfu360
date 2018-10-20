a40e246e7b837718622d9e63829cb610334edcb7
cnculture <it@zhuxia.ltd>
2018-10-20 22:19:16 +0800

    first commit

---
+++ precache-manifest.7703e651e79eb36855aaa99d588a11cf.js
@@ -0,0 +1,124 @@
+self.__precacheManifest = [
+  {
+    "revision": "92aba9c922b3391b57d6",
+    "url": "/hanfu360/css/question.a18c8a94.css"
+  },
+  {
+    "revision": "e3115184c21ceef3f6b26d809cea732b",
+    "url": "/hanfu360/libs/strophe-1.2.8.min.js"
+  },
+  {
+    "revision": "786dffd8044534f68f8af3dc58656b1b",
+    "url": "/hanfu360/plus.png"
+  },
+  {
+    "revision": "e87200ec4c5618c070b6",
+    "url": "/hanfu360/css/rank.61c8556b.css"
+  },
+  {
+    "url": "/hanfu360/js/chunk-vendors-16469003995c093e58e8.js"
+  },
+  {
+    "url": "/hanfu360/js/feed-f42f458b110c8a02e504.js"
+  },
+  {
+    "url": "/hanfu360/js/wallet-37f18e853c0c7607e17a.js"
+  },
+  {
+    "url": "/hanfu360/js/feed~news-129b7e1af852c8f5e783.js"
+  },
+  {
+    "url": "/hanfu360/js/rank-e87200ec4c5618c070b6.js"
+  },
+  {
+    "url": "/hanfu360/js/group-9f87108d0d0d21edc2d4.js"
+  },
+  {
+    "url": "/hanfu360/js/question-92aba9c922b3391b57d6.js"
+  },
+  {
+    "url": "/hanfu360/js/message-380d0999a87b4f8b8106.js"
+  },
+  {
+    "url": "/hanfu360/js/post-a1b8c71f5f2212d1309a.js"
+  },
+  {
+    "url": "/hanfu360/js/news-4c77e8084126242bff9b.js"
+  },
+  {
+    "revision": "1b80cde03faf06ffd08b8a64e8f707ff",
+    "url": "/hanfu360/img/pic_default_man.1b80cde0.png"
+  },
+  {
+    "revision": "a1b8c71f5f2212d1309a",
+    "url": "/hanfu360/css/post.e6c16eda.css"
+  },
+  {
+    "revision": "58b75c6f72f26bf50b1d",
+    "url": "/hanfu360/js/app.24c57961.js"
+  },
+  {
+    "revision": "193836cf54ca966d46bc26c3e55cf138",
+    "url": "/hanfu360/index.html"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/hanfu360/img/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "a9c07f4ac7de1fad803f4755de893092",
+    "url": "/hanfu360/img/user_home_default_cover.a9c07f4a.png"
+  },
+  {
+    "revision": "37f18e853c0c7607e17a",
+    "url": "/hanfu360/css/wallet.83b4e0c3.css"
+  },
+  {
+    "revision": "df39055cc5065a8ef03531aa56fe8c3f",
+    "url": "/hanfu360/img/logo_thinksns+@2x.df39055c.png"
+  },
+  {
+    "revision": "1ac08eca22770cf6d64f93207de53081",
+    "url": "/hanfu360/img/pic_default_secret.1ac08eca.png"
+  },
+  {
+    "revision": "0bcff4e589e5cc244f83163513570485",
+    "url": "/hanfu360/img/img_default_search@2x.0bcff4e5.png"
+  },
+  {
+    "revision": "8dc4fad355a26aadcfc6b7b6710cffac",
+    "url": "/hanfu360/img/pic_default_woman.8dc4fad3.png"
+  },
+  {
+    "revision": "1d7e5008cb945f72b5c2cd3b9d030793",
+    "url": "/hanfu360/img/ico_ts_assistant@2x.1d7e5008.png"
+  },
+  {
+    "revision": "4c77e8084126242bff9b",
+    "url": "/hanfu360/css/news.457de040.css"
+  },
+  {
+    "revision": "380d0999a87b4f8b8106",
+    "url": "/hanfu360/css/message.79012a2c.css"
+  },
+  {
+    "revision": "9f87108d0d0d21edc2d4",
+    "url": "/hanfu360/css/group.7329d971.css"
+  },
+  {
+    "revision": "129b7e1af852c8f5e783",
+    "url": "/hanfu360/css/feed~news.63d33135.css"
+  },
+  {
+    "revision": "f42f458b110c8a02e504",
+    "url": "/hanfu360/css/feed.836c7b23.css"
+  },
+  {
+    "revision": "16469003995c093e58e8",
+    "url": "/hanfu360/css/chunk-vendors.0a30ea03.css"
+  },
+  {
+    "revision": "58b75c6f72f26bf50b1d",
+    "url": "/hanfu360/css/app.199ac81a.css"
+  }
+];
